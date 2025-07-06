6cf89281d0bb8e915c24b1ada04b6c246d756543
DhruvBadukle <badukledhruv@gmail.com>
2025-07-06 22:12:36 +0530

    4

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+import Frameworks from "../components/Frameworks";
 import coding from "../assets/coding-pov.png";
 import csharp from "../assets/logos/java.png";
 import dotnet from "../assets/logos/dotnet-pink.png";
